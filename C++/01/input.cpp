81cae3489b29dc12c7e5ef65d5049bfb9d02887d
mayank-vashishtt <mayankvashishtt@gmail.com>
2025-08-23 21:52:56 +0530

    hft class 1

---
+++ C++/01/input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// using namespace std;
+
+int main(){
+
+
+    int a,b;
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
+
+    // std::cout << "Hello, World!" << endl;
+
+
+
+    return 0; 
+
+
+}
